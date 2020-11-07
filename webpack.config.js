1c557b042b4304cfdcf1175d8d712a6564f1040f
Arslanoov <rasularslanoov@gmail.com>
2020-11-07 18:29:20 +0500

    Fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,71 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: "development",
 
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    resolve: {
+        extensions: [".js", ".css", ".scss", ".sass"]
+    },
+
+    entry: [
+        './src/js/index.js',
+        './src/scss/index.scss'
+    ],
 
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Web Paint',
-            template: 'public/index.html'
-        })
+            template: 'src/html/views/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+        /*new CopyWebpackPlugin([{
+            patterns: [
+                {
+                    from: './src/fonts',
+                    to: './fonts'
+                },
+                {
+                    from: './src/favicon',
+                    to: './favicon'
+                },
+                {
+                    from: './src/img',
+                    to: './img'
+                },
+                {
+                    from: './src/uploads',
+                    to: './uploads'
+                }
+            ]
+        }])*/
     ],
 
+    module: {
+        rules: [
+            // Loading CSS
+            {
+                test: /\.(css)$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader']
+            },
+
+            // Loading SASS/SCSS
+            {
+                test: /\.(s[ca]ss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
     devServer: {
+        contentBase: path.join(__dirname, 'dist'),
         open: true,
         historyApiFallback: true
     }
